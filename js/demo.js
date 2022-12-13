ace59f9b4e434d870d69b08dd6b359f3d6e59a38
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2022-12-13 19:52:23 +0200

    Change to local library

--- js/demo.js
+++ js/demo.js
@@ -3,7 +3,7 @@ import { URLs, MarkdownBlock, MarkdownSpan, MarkdownElement } from "./md-block.j
 import create from "https://v2.blissfuljs.com/src/dom/create.js";
 
 // const importURL = new URL("md-block.js", location) + "";
-const importURL = "https://md-block.verou.me/md-block.js";
+const importURL = "./md-block.js";
 let js = `import {MarkdownBlock, MarkdownSpan, MarkdownElement} from "${importURL}";`;
 
 for (let script of document.querySelectorAll("script[type='text/html'].md-block-demo")) {
