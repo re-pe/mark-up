d484e326102fb027f7acc821a43a58cc889254eb
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2022-12-13 18:16:54 +0200

    Pretify files

--- js/demo.js
+++ js/demo.js
@@ -1,4 +1,4 @@
-import {URLs, MarkdownBlock, MarkdownSpan, MarkdownElement} from "./md-block.js";
+import { URLs, MarkdownBlock, MarkdownSpan, MarkdownElement } from "./md-block.js";
 
 import create from "https://v2.blissfuljs.com/src/dom/create.js";
 
@@ -7,107 +7,107 @@ const importURL = "https://md-block.verou.me/md-block.js";
 let js = `import {MarkdownBlock, MarkdownSpan, MarkdownElement} from "${importURL}";`;
 
 for (let script of document.querySelectorAll("script[type='text/html'].md-block-demo")) {
-	let html = script.textContent;
+  let html = script.textContent;
 
-	html = Prism?.plugins.NormalizeWhitespace?.normalize(html);
-	let description = script.previousElementSibling;
+  html = Prism?.plugins.NormalizeWhitespace?.normalize(html);
+  let description = script.previousElementSibling;
 
-	let exampleContainer = create({
-		tag: "article",
-		className: "demo-container",
-		contents: [
-			{
-				tag: "header",
-				contents: [
-					description,
-					{
-						tag: "form",
-						action: "https://codepen.io/pen/define",
-						method: "POST",
-						target: "_blank",
-						contents: [
-							{
-								tag: "input",
-								type: "hidden",
-								name: "data",
-								value: JSON.stringify({
-									title: description.textContent,
-									html,
-									css: "",
-									js,
-									editors: "1110",
-									// head: `<script type="module" src="${importURL}"></script>`
-								})
-							},
-							{
-								tag: "button",
-								textContent: "Edit in CodePen ↗️"
-							}
-						],
-						after: script,
-					}
-				]
-			},
-			{
-				tag: "pre",
-				class: "language-html demo-code",
-				contents: {
-					tag: "code",
-					textContent: html
-				}
-			},
-			{
-				className: "demo-output",
-				innerHTML: html
-			}
-		],
-		after: script
-	});
+  let exampleContainer = create({
+    tag: "article",
+    className: "demo-container",
+    contents: [
+      {
+        tag: "header",
+        contents: [
+          description,
+          {
+            tag: "form",
+            action: "https://codepen.io/pen/define",
+            method: "POST",
+            target: "_blank",
+            contents: [
+              {
+                tag: "input",
+                type: "hidden",
+                name: "data",
+                value: JSON.stringify({
+                  title: description.textContent,
+                  html,
+                  css: "",
+                  js,
+                  editors: "1110",
+                  // head: `<script type="module" src="${importURL}"></script>`
+                })
+              },
+              {
+                tag: "button",
+                textContent: "Edit in CodePen ↗️"
+              }
+            ],
+            after: script,
+          }
+        ]
+      },
+      {
+        tag: "pre",
+        class: "language-html demo-code",
+        contents: {
+          tag: "code",
+          textContent: html
+        }
+      },
+      {
+        className: "demo-output",
+        innerHTML: html
+      }
+    ],
+    after: script
+  });
 }
 
 import * as Stretchy from "https://stretchy.verou.me/dist/stretchy.min.js";
 Stretchy.selectors.filter = "#repl *";
 Stretchy.init();
 
-if (localStorage["md-block-demo"]){
-	JSON.parse(localStorage["md-block-demo"]).forEach(([name, value]) => {
-		let el = repl_container.elements[name];
+if (localStorage["md-block-demo"]) {
+  JSON.parse(localStorage["md-block-demo"]).forEach(([name, value]) => {
+    let el = repl_container.elements[name];
 
-		if (el) {
-			el.value = value;
-			el.dispatchEvent(new Event("input"));
-		}
-	});
+    if (el) {
+      el.value = value;
+      el.dispatchEvent(new Event("input"));
+    }
+  });
 }
 
 repl.addEventListener("input", evt => {
-	let {target} = evt;
-	let f = repl_container;
-	let html = `<${f.tag.value} ${f.attributes.value}>
+  let { target } = evt;
+  let f = repl_container;
+  let html = `<${f.tag.value} ${f.attributes.value}>
 ${f.contents.value}
 </${f.tag.value}>`;
-// console.log(html);
-	end_tag.textContent = f.tag.value;
+  // console.log(html);
+  end_tag.textContent = f.tag.value;
 
-	rendering.innerHTML = html;
+  rendering.innerHTML = html;
 
-	let values = [];
-	for (let el of f.elements) {
-		if (el.name) {
-			values.push([el.name, el.value]);
-		}
-	}
+  let values = [];
+  for (let el of f.elements) {
+    if (el.name) {
+      values.push([el.name, el.value]);
+    }
+  }
 
-	if (evt.isTrusted) {
-		localStorage["md-block-demo"] = JSON.stringify(values);
-	}
+  if (evt.isTrusted) {
+    localStorage["md-block-demo"] = JSON.stringify(values);
+  }
 
 });
 
 repl.dispatchEvent(new Event("input"));
 
 rendering.addEventListener("md-render", evt => {
-	let code = output_html.querySelector("code");
-	code.textContent = evt.target.outerHTML;
-	Prism.highlightElement(code);
+  let code = output_html.querySelector("code");
+  code.textContent = evt.target.outerHTML;
+  Prism.highlightElement(code);
 });
--- js/md-block.js
+++ js/md-block.js
@@ -8,292 +8,292 @@ let DOMPurify = window.DOMPurify;
 let Prism = window.Prism;
 
 export const URLs = {
-	marked: "https://cdn.jsdelivr.net/npm/marked/src/marked.min.js",
-	DOMPurify: "https://cdn.jsdelivr.net/npm/dompurify@2.3.3/dist/purify.es.min.js"
+  marked: "https://cdn.jsdelivr.net/npm/marked/src/marked.min.js",
+  DOMPurify: "https://cdn.jsdelivr.net/npm/dompurify@2.3.3/dist/purify.es.min.js"
 }
 
 // Fix indentation
 function deIndent(text) {
-	let indent = text.match(/^[\r\n]*([\t ]+)/);
+  let indent = text.match(/^[\r\n]*([\t ]+)/);
 
-	if (indent) {
-		indent = indent[1];
+  if (indent) {
+    indent = indent[1];
 
-		text = text.replace(RegExp("^" + indent, "gm"), "");
-	}
+    text = text.replace(RegExp("^" + indent, "gm"), "");
+  }
 
-	return text;
+  return text;
 }
 
 export class MarkdownElement extends HTMLElement {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.renderer = Object.assign({}, this.constructor.renderer);
+    this.renderer = Object.assign({}, this.constructor.renderer);
 
-		for (let property in this.renderer) {
-			this.renderer[property] = this.renderer[property].bind(this);
-		}
-	}
+    for (let property in this.renderer) {
+      this.renderer[property] = this.renderer[property].bind(this);
+    }
+  }
 
-	get rendered() {
-		return this.getAttribute("rendered");
-	}
+  get rendered() {
+    return this.getAttribute("rendered");
+  }
 
-	get mdContent () {
-		return this._mdContent;
-	}
+  get mdContent() {
+    return this._mdContent;
+  }
 
-	set mdContent (html) {
-		this._mdContent = html;
-		this._contentFromHTML = false;
+  set mdContent(html) {
+    this._mdContent = html;
+    this._contentFromHTML = false;
 
-		this.render();
-	}
+    this.render();
+  }
 
-	connectedCallback() {
-		Object.defineProperty(this, "untrusted", {
-			value: this.hasAttribute("untrusted"),
-			enumerable: true,
-			configurable: false,
-			writable: false
-		});
+  connectedCallback() {
+    Object.defineProperty(this, "untrusted", {
+      value: this.hasAttribute("untrusted"),
+      enumerable: true,
+      configurable: false,
+      writable: false
+    });
 
-		if (this._mdContent === undefined) {
-			this._contentFromHTML = true;
-			this._mdContent = deIndent(this.innerHTML);
-		}
+    if (this._mdContent === undefined) {
+      this._contentFromHTML = true;
+      this._mdContent = deIndent(this.innerHTML);
+    }
 
-		this.render();
-	}
+    this.render();
+  }
 
-	async render () {
-		if (!this.isConnected || this._mdContent === undefined) {
-			return;
-		}
+  async render() {
+    if (!this.isConnected || this._mdContent === undefined) {
+      return;
+    }
 
-		if (!marked) {
-			marked = import(URLs.marked).then(m => m.marked);
-		}
+    if (!marked) {
+      marked = import(URLs.marked).then(m => m.marked);
+    }
 
-		marked = await marked;
+    marked = await marked;
 
-		marked.setOptions({
-			gfm: true,
-			smartypants: true,
-			langPrefix: "language-",
-		});
+    marked.setOptions({
+      gfm: true,
+      smartypants: true,
+      langPrefix: "language-",
+    });
 
-		marked.use({renderer: this.renderer});
+    marked.use({ renderer: this.renderer });
 
-		let html = this._parse();
+    let html = this._parse();
 
-		if (this.untrusted) {
-			let mdContent = this._mdContent;
-			html = await MarkdownElement.sanitize(html);
-			if (this._mdContent !== mdContent) {
-				// While we were running this async call, the content changed
-				// We don’t want to overwrite with old data. Abort mission!
-				return;
-			}
-		}
+    if (this.untrusted) {
+      let mdContent = this._mdContent;
+      html = await MarkdownElement.sanitize(html);
+      if (this._mdContent !== mdContent) {
+        // While we were running this async call, the content changed
+        // We don’t want to overwrite with old data. Abort mission!
+        return;
+      }
+    }
 
-		this.innerHTML = html;
+    this.innerHTML = html;
 
-		if (!Prism && URLs.Prism && this.querySelector("code")) {
-			Prism = import(URLs.Prism);
+    if (!Prism && URLs.Prism && this.querySelector("code")) {
+      Prism = import(URLs.Prism);
 
-			if (URLs.PrismCSS) {
-				let link = document.createElement("link");
-				link.rel = "stylesheet";
-				link.href = URLs.PrismCSS;
-				document.head.appendChild(link);
-			}
-		}
+      if (URLs.PrismCSS) {
+        let link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = URLs.PrismCSS;
+        document.head.appendChild(link);
+      }
+    }
 
-		if (Prism) {
-			await Prism; // in case it's still loading
-			Prism.highlightAllUnder(this);
-		}
+    if (Prism) {
+      await Prism; // in case it's still loading
+      Prism.highlightAllUnder(this);
+    }
 
-		if (this.src) {
-			this.setAttribute("rendered", this._contentFromHTML? "fallback" : "remote");
-		}
-		else {
-			this.setAttribute("rendered", this._contentFromHTML? "content" : "property");
-		}
+    if (this.src) {
+      this.setAttribute("rendered", this._contentFromHTML ? "fallback" : "remote");
+    }
+    else {
+      this.setAttribute("rendered", this._contentFromHTML ? "content" : "property");
+    }
 
-		// Fire event
-		let event = new CustomEvent("md-render", {bubbles: true, composed: true});
-		this.dispatchEvent(event);
-	}
+    // Fire event
+    let event = new CustomEvent("md-render", { bubbles: true, composed: true });
+    this.dispatchEvent(event);
+  }
 
-	static async sanitize(html) {
-		if (!DOMPurify) {
-			DOMPurify = import(URLs.DOMPurify).then(m => m.default);
-		}
+  static async sanitize(html) {
+    if (!DOMPurify) {
+      DOMPurify = import(URLs.DOMPurify).then(m => m.default);
+    }
 
-		DOMPurify = await DOMPurify; // in case it's still loading
+    DOMPurify = await DOMPurify; // in case it's still loading
 
-		return DOMPurify.sanitize(html);
-	}
+    return DOMPurify.sanitize(html);
+  }
 };
 
 export class MarkdownSpan extends MarkdownElement {
-	constructor() {
-		super();
-	}
+  constructor() {
+    super();
+  }
 
-	_parse () {
-		return marked.parseInline(this._mdContent);
-	}
+  _parse() {
+    return marked.parseInline(this._mdContent);
+  }
 
-	static renderer = {
-		codespan (code) {
-			if (this._contentFromHTML) {
-				// Inline HTML code needs to be escaped to not be parsed as HTML by the browser
-				// This results in marked double-escaping it, so we need to unescape it
-				code = code.replace(/&amp;(?=[lg]t;)/g, "&");
-			}
-			else {
-				// Remote code may include characters that need to be escaped to be visible in HTML
-				code = code.replace(/</g, "&lt;");
-			}
+  static renderer = {
+    codespan(code) {
+      if (this._contentFromHTML) {
+        // Inline HTML code needs to be escaped to not be parsed as HTML by the browser
+        // This results in marked double-escaping it, so we need to unescape it
+        code = code.replace(/&amp;(?=[lg]t;)/g, "&");
+      }
+      else {
+        // Remote code may include characters that need to be escaped to be visible in HTML
+        code = code.replace(/</g, "&lt;");
+      }
 
-			return `<code>${code}</code>`;
-		}
-	}
+      return `<code>${code}</code>`;
+    }
+  }
 }
 
 export class MarkdownBlock extends MarkdownElement {
-	constructor() {
-		super();
-	}
+  constructor() {
+    super();
+  }
 
-	get src() {
-		return this._src;
-	}
+  get src() {
+    return this._src;
+  }
 
-	set src(value) {
-		this.setAttribute("src", value);
-	}
+  set src(value) {
+    this.setAttribute("src", value);
+  }
 
-	get hmin() {
-		return this._hmin || 1;
-	}
+  get hmin() {
+    return this._hmin || 1;
+  }
 
-	set hmin(value) {
-		this.setAttribute("hmin", value);
-	}
+  set hmin(value) {
+    this.setAttribute("hmin", value);
+  }
 
-	get hlinks() {
-		return this._hlinks ?? null;
-	}
+  get hlinks() {
+    return this._hlinks ?? null;
+  }
 
-	set hlinks(value) {
-		this.setAttribute("hlinks", value);
-	}
+  set hlinks(value) {
+    this.setAttribute("hlinks", value);
+  }
 
-	_parse () {
-		return marked.parse(this._mdContent);
-	}
+  _parse() {
+    return marked.parse(this._mdContent);
+  }
 
-	static renderer = Object.assign({
-		heading (text, level, _raw, slugger) {
-			level = Math.min(6, level + (this.hmin - 1));
-			const id = slugger.slug(text);
-			const hlinks = this.hlinks;
+  static renderer = Object.assign({
+    heading(text, level, _raw, slugger) {
+      level = Math.min(6, level + (this.hmin - 1));
+      const id = slugger.slug(text);
+      const hlinks = this.hlinks;
 
-			let content;
+      let content;
 
-			if (hlinks === null) {
-				// No heading links
-				content = text;
-			}
-			else {
-				content = `<a href="#${id}" class="anchor">`;
+      if (hlinks === null) {
+        // No heading links
+        content = text;
+      }
+      else {
+        content = `<a href="#${id}" class="anchor">`;
 
-				if (hlinks === "") {
-					// Heading content is the link
-					content += text + "</a>";
-				}
-				else {
-					// Headings are prepended with a linked symbol
-					content += hlinks + "</a>" + text;
-				}
-			}
+        if (hlinks === "") {
+          // Heading content is the link
+          content += text + "</a>";
+        }
+        else {
+          // Headings are prepended with a linked symbol
+          content += hlinks + "</a>" + text;
+        }
+      }
 
-			return `
+      return `
 				<h${level} id="${id}">
 					${content}
 				</h${level}>`;
-		},
+    },
 
-		code (code, language, escaped) {
-			if (this._contentFromHTML) {
-				// Inline HTML code needs to be escaped to not be parsed as HTML by the browser
-				// This results in marked double-escaping it, so we need to unescape it
-				code = code.replace(/&amp;(?=[lg]t;)/g, "&");
-			}
-			else {
-				// Remote code may include characters that need to be escaped to be visible in HTML
-				code = code.replace(/</g, "&lt;");
-			}
+    code(code, language, escaped) {
+      if (this._contentFromHTML) {
+        // Inline HTML code needs to be escaped to not be parsed as HTML by the browser
+        // This results in marked double-escaping it, so we need to unescape it
+        code = code.replace(/&amp;(?=[lg]t;)/g, "&");
+      }
+      else {
+        // Remote code may include characters that need to be escaped to be visible in HTML
+        code = code.replace(/</g, "&lt;");
+      }
 
-			return `<pre class="language-${language}"><code>${code}</code></pre>`;
-		}
-	}, MarkdownSpan.renderer);
+      return `<pre class="language-${language}"><code>${code}</code></pre>`;
+    }
+  }, MarkdownSpan.renderer);
 
-	static get observedAttributes() {
-		return ["src", "hmin", "hlinks"];
-	}
+  static get observedAttributes() {
+    return ["src", "hmin", "hlinks"];
+  }
 
-	attributeChangedCallback(name, oldValue, newValue) {
-		if (oldValue === newValue) {
-			return;
-		}
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
 
-		switch (name) {
-			case "src":
-				let url;
-				try {
-					url = new URL(newValue, location);
-				}
-				catch (e) {
-					return;
-				}
+    switch (name) {
+      case "src":
+        let url;
+        try {
+          url = new URL(newValue, location);
+        }
+        catch (e) {
+          return;
+        }
 
-				let prevSrc = this.src;
-				this._src = url;
+        let prevSrc = this.src;
+        this._src = url;
 
-				if (this.src !== prevSrc) {
-					fetch(this.src)
-					.then(response => {
-						if (!response.ok) {
-							throw new Error(`Failed to fetch ${this.src}: ${response.status} ${response.statusText}`);
-						}
+        if (this.src !== prevSrc) {
+          fetch(this.src)
+            .then(response => {
+              if (!response.ok) {
+                throw new Error(`Failed to fetch ${this.src}: ${response.status} ${response.statusText}`);
+              }
 
-						return response.text();
-					})
-					.then(text => {
-						this.mdContent = text;
-					})
-					.catch(e => {});
-				}
+              return response.text();
+            })
+            .then(text => {
+              this.mdContent = text;
+            })
+            .catch(e => { });
+        }
 
-				break;
-			case "hmin":
-				if (newValue > 0) {
-					this._hmin = +newValue;
+        break;
+      case "hmin":
+        if (newValue > 0) {
+          this._hmin = +newValue;
 
-					this.render();
-				}
-				break;
-			case "hlinks":
-				this._hlinks = newValue;
-				this.render();
-		}
-	}
+          this.render();
+        }
+        break;
+      case "hlinks":
+        this._hlinks = newValue;
+        this.render();
+    }
+  }
 }
 
 
