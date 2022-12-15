99a3e221b7b3416fa2f69a68cbe630f60f12574e
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2022-12-15 09:29:10 +0200

    Clone files for example with remark

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ import './lib/prism-live.js';
 const { splitPathName } = await import("./helpers.js");
 const fileName = splitPathName().fileName;
 
-if ( fileName === 'index.html') {
+if (fileName === 'index.html') {
   import('./md-block.js')
-} else if ( fileName === 'index-mrkd.html') {
+} else if (fileName === 'index-mrkd.html') {
   import('./demo.js');
+} else if (fileName === 'index-rmrk.html') {
+  import('./demo-rmrk.js');
 }
