f610d5dab57eb55bf6974580f6ad40c6647911bb
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2022-12-18 01:55:40 +0200

    Move asciidoctor libraries to separate folder

--- js/prepareAsciidoctor.js
+++ js/prepareAsciidoctor.js
@@ -1,5 +1,5 @@
-await import ("./lib/asciidoctor.js");
-await import ("./lib/asciidoctor-emoji.js");
+await import ("./lib/asciidoctor/asciidoctor.js");
+await import ("./lib/asciidoctor/asciidoctor-emoji.js");
 const asciidoctor = Asciidoctor();
 const emoji = AsciidoctorEmoji;
 const asciidoctorExtensions = asciidoctor.Extensions.create();
